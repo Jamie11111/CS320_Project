7380a7ce9a04b268289b4743a7f6132df2ec035e
Yellowly <irawizza@gmail.com>
2026-03-26 14:17:56 -0400

    Set up route structure and some basic examples.

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
+import { accountRoutes } from "./routes/account";
+import { chatRoutes } from "./routes/chats";
+import { listingRoutes } from "./routes/posts";
+import { userRoutes } from "./routes/users";
+
 console.log("Hello via Bun!");
+
+Bun.serve({
+    port: 3000,
+    routes: {
+        ...listingRoutes,
+        ...userRoutes,
+        ...accountRoutes,
+        ...chatRoutes,
+        "/*": () => new Response("Not Found", {status: 404})
+    }
+});
+
+console.log("App is running on port 3000");
