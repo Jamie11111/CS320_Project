fbf732e6581073574d91aac7f3ad2473a7be371f
Yellowly <irawizza@gmail.com>
2026-03-30 14:30:42 -0400

    Added to database example

--- database/example.ts
+++ database/example.ts
@@ -1,5 +1,5 @@
 // This code would go in some main or index file 
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL!
 const supabaseKey = process.env.SUPABASE_ANON_KEY!
@@ -7,8 +7,12 @@ const supabaseKey = process.env.SUPABASE_ANON_KEY!
 // This client is accessed by index.ts and passed to routes that need to interact with the database.
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
+// All the functions below would be implemented in their own separate file
+// Hence why `supabase` is passed in as an argument
+// I've done this because we need to be able to easily swap out the supabase client to use a test database
+
 // Example sign-in function
-async function signIn(email: string, password: string) {
+async function signIn(supabase: SupabaseClient, email: string, password: string) {
   const { data: {user}, error } = await supabase.auth.signInWithPassword({
     email,
     password,
@@ -18,7 +22,7 @@ async function signIn(email: string, password: string) {
 }
 
 // Example of verifying an auth token from a route
-async function verifyAuthToken(request: Request) {
+async function verifyAuthToken(supabase: SupabaseClient, request: Request) {
   const authHeader = request.headers.get('Authorization');
   if (!authHeader) {
     return null; // No token provided
@@ -38,4 +42,18 @@ async function verifyAuthToken(request: Request) {
   }
 
   return user;
+}
+
+// Example of getting a listing from the database
+async function getListingById(supabase: SupabaseClient, listingId: number) {
+  const { data, error } = await supabase
+    .from('listings')
+    .select('*')
+    .eq('id', listingId)
+    .single();  
+    if (error) {
+        console.error('Error fetching listing:', error.message);
+        return null;
+    }
+    return data;
 }
